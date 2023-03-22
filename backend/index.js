45320c6a8120dbee56623135e1bd45a8aeb500e5
Alisha-0704 <70397438+Alisha-0704@users.noreply.github.com>
2023-03-23 02:42:34 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const PORT = 8000
+const express = require("express")
+const {MongoClient} = require('mongodb')
+const uri = 'mongodb+srv://A0lish7_:A0lish7@clusternoob.umtu1w6.mongodb.net/?retryWrites=true&w=majority'
+
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.json('Hello to my app')
+})
+
+app.post('/signup', (req, res) =>{
+    res.json('Hello to my app')
+})
+app.get('/users', async (req, res) =>{
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    try {
+        await client.connect()
+        const database = client.db('AmiStudy')
+        const users = database.collection('users')
+
+        const returnedusers = await users.find().toArray()
+        res.send(returnedusers)
+    } finally {
+        await client.close()
+    }
+})
+
+app.listen(PORT, () => console.log('server running on port ' + PORT))
